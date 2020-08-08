1af9af52e736c5d452ce7295f90d961991544da2
EmmanuelEffi <emmanueleffi03@gmail.com>
2020-08-08 14:41:42 +0100

    Added mail functionality, changed the grid alignments of the portfolio

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+
+    if($_POST["Hi"]) {
+
+    mail("emmanueleffi03@gmail.com", "Here is the subject line",
+
+    $_POST["insert your message here"]. "From: an@email.address");
+
+    }
+
+?>
